0a925c8ec6417144b628dd5b631b94e45d9601fc
mahha74 <50704079+mahha74@users.noreply.github.com>
2023-04-24 16:54:38 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { UserProvider } from "./providers/UserProvider";
+import { Router } from "./router/Router";
+import "./styles.css";
+import { RecoilRoot } from "recoil";
+
+export default function App() {
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />;
+      </UserProvider>
+    </RecoilRoot>
+  );
+}
